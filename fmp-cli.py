a5f1dad029acc4397d92badd57ddef00f0169550
Easlee Liu <easlee@outlook.com>
2022-08-18 19:59:21 +0800

    mark 1.18.3

--- fmp-cli.py
+++ fmp-cli.py
@@ -169,7 +169,7 @@ def useYaml(_version):
                 print("!!! 手动运行unity2021/copy-dll.bat更新依赖库")
 
 
-version = "1.18.2"
+version = "1.18.3"
 print("****************************************************")
 print("* FMP Client - ver {}".format(version))
 print("****************************************************")
